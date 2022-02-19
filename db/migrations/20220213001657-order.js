008e441febf9bb1c1872b74d260345e3f5b630ad
Miguel <migueleduagui@gmail.com>
2022-02-18 22:41:29 -0300

    fix migrations order

--- db/migrations/20220213001657-order.js
+++ db/migrations/20220213001657-order.js
@@ -1,9 +1,34 @@
 'use strict';
-const { ORDER_TABLE, OrderSchema } = require('../models/order.model');
+const { ORDER_TABLE } = require('../models/order.model');
 
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE,{
+      id:{
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      customerId:{
+        allowNull:false,
+        type: DataTypes.INTEGER,
+        field: 'customer_id',
+        references:{
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+
+      },
+      createdAt:{
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW
+      }
+    });
   },
 
   async down(queryInterface) {

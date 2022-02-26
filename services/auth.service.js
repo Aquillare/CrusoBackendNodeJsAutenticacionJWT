7f3c20c01b3e510e0d6d7b843a9b885da8313081
Miguel <migueleduagui@gmail.com>
2022-02-26 15:44:11 -0300

    fix auth service

--- services/auth.service.js
+++ services/auth.service.js
@@ -2,7 +2,6 @@ const boom = require('@hapi/boom');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
-const { classToInvokable } = require('sequelize/dist/lib/utils');
 
 const config = require('../config/config');
 

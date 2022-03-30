5a59747d5a1caddae75b852baf77bd1ca2bb1446
Miguel <migueleduagui@gmail.com>
2022-03-30 19:08:34 -0300

    fix

--- db/config.js
+++ db/config.js
@@ -8,6 +8,10 @@ module.exports = {
   development: {
     url: config.dbUrl,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl:{
+        rejectUnauthorized : false
+      }
   },
   production: {
     url: config.dbUrl,

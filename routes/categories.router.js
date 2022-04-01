3c0cd7253c385ee9a53c9f29f95aa45b1ff48af0
Miguel <migueleduagui@gmail.com>
2022-04-01 00:08:25 -0300

    change aws region

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -22,7 +22,7 @@ const spacesEndpoint = new AWS.Endpoint(config.cloudEndpoint);
 
 const s3 = new AWS.S3({
   endpoint:spacesEndpoint,
-  region:'us-west-2',
+  region:'us-west-1',
   s3BucketEndpoint: true,
 
 });

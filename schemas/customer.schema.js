e8ffd4207525e6b7bebfadf87002d25aa6ec2d89
Miguel <migueleduagui@gmail.com>
2022-04-13 18:31:00 -0300

    migracion para modificar un campo de la tabla customers

--- schemas/customer.schema.js
+++ schemas/customer.schema.js
@@ -3,10 +3,10 @@ const Joi = require('joi');
 const id = Joi.number().integer();
 const name = Joi.string().min(3).max(30);
 const lastName = Joi.string().min(3).max(20);
-const phone = Joi.number().integer();
+const phone = Joi.string().max(10);
 const userId = Joi.number().integer();
 const email = Joi.string().email();
-const password = Joi.string();
+const password = Joi.string().min(4).max(16);
 
 const createCustomerSchema = Joi.object({
   name: name.required(),
